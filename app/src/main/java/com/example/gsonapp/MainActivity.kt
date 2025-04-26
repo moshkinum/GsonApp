11aec82ea0588c438a6565c1762a41622e17a3fc
Yury Moshkin <yumoshkin@gmail.com>
2025-04-26 11:34:16 +0300

    Commit 2

--- app/src/main/java/com/example/gsonapp/MainActivity.kt
+++ app/src/main/java/com/example/gsonapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gsonapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRun = findViewById<Button>(R.id.btnRun)
+        btnRun.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Клик на кнопке", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
